ad99f28474546f85049beb9e051aef091a44b7f2
nyrf <xiaojin0913@163.com>
2024-05-17 03:39:28 +0800

    Update diy-part1.sh

--- OpenWrt/Redmi-AX6000/diy-part1.sh
+++ OpenWrt/Redmi-AX6000/diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# cd $WORK_PATH 目录下,先运行的 public.h -> 设备.h -> scripts/feeds install -a
+# 必须的文件
+sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
