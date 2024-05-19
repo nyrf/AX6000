3c130ea57ff90ddd740719b33c25696a47b3958d
nyrf <xiaojin0913@163.com>
2024-05-19 16:47:42 +0800

    add lean

---
+++ Lean/diy-part1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# cd $WORK_PATH 目录下,先运行的 public.h -> 设备.h -> scripts/feeds install -a
+# 必须的文件
+#sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+./scripts/feeds update -a
+./scripts/feeds install -a -f -p kenzo
+./scripts/feeds install -a -f -p small
+
+# git clone https://github.com/m0eak/luci-app-turboacc-mtk package/luci-app-turboacc-mtk
+# sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
+# ./scripts/feeds update -i && ./scripts/feeds install -a
---
+++ Lean/diy-part2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+# echo "设置lan ip"
+# lan_ip='192.168.1.2' 
+# sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+# echo "修改时区"
+# utc_name='Asia\/Shanghai' 
+# sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+# echo "修改机器名称"
+# device_name='OpenWrt'
+# sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+# echo "修改wifi名称"
+# wifi_name=ysf
+# sed -i "s/OpenWrt/$wifi_name/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+
+# echo "wifi默认开启"
+# sed -i '/set wireless.${name}.disabled/d' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
