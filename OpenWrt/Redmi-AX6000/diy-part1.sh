65d641ca26d3ca866620516dfc17b5cbb1e62c18
nyrf <xiaojin0913@163.com>
2024-05-23 05:56:45 +0800

    use latest golang

--- OpenWrt/Redmi-AX6000/diy-part1.sh
+++ OpenWrt/Redmi-AX6000/diy-part1.sh
@@ -23,6 +23,9 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a -f -p kenzo
 ./scripts/feeds install -a -f -p small
